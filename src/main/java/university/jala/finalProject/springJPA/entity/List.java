c5338bec94c5de73d11296ab67a4207f8218a1e1
Melisa Peano <melisa.peano0125@jala.university>
2025-09-24 16:46:37 -0300

    add JPA configuration and entities

---
+++ src/main/java/university/jala/finalProject/springJPA/entity/List.java
@@ -0,0 +1,67 @@
+package university.jala.finalProject.springJPA;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Entity
+@Table(name = "List", schema = "databaseII")
+public class List {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "list_id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @Column(name = "list_name", nullable = false, length = 60)
+    private String listName;
+
+    @Column(name = "list_description")
+    private String listDescription;
+
+    @Column(name = "created_in")
+    private Instant createdIn;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getListName() {
+        return listName;
+    }
+
+    public void setListName(String listName) {
+        this.listName = listName;
+    }
+
+    public String getListDescription() {
+        return listDescription;
+    }
+
+    public void setListDescription(String listDescription) {
+        this.listDescription = listDescription;
+    }
+
+    public Instant getCreatedIn() {
+        return createdIn;
+    }
+
+    public void setCreatedIn(Instant createdIn) {
+        this.createdIn = createdIn;
+    }
+
+}
