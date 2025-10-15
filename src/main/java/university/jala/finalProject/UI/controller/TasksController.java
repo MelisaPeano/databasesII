0a5de0df42181a1b929211c467411da80a75050f
Antonio Binda <Antonio.Binda0125@jala.university>
2025-10-14 22:57:57 -0300

    fix(auth,ui): open dashboard after login; fix FXML Insets

---
+++ src/main/java/university/jala/finalProject/UI/controller/TasksController.java
@@ -0,0 +1,13 @@
+package university.jala.finalProject.UI.controller;
+
+import org.springframework.stereotype.Component;
+import javafx.fxml.FXML;
+
+@Component
+public class TasksController {
+
+    @FXML
+    public void initialize() {
+
+    }
+}
